f466cbd7cb0935943db0f14ffe8efb7d7cc2d5b6
Tomás <tomas.benitez.silva@gmail.com>
2021-11-30 19:10:04 -0300

    added font weight 900 for roboto

--- pages/_app.js
+++ pages/_app.js
@@ -22,7 +22,7 @@ const MyApp = ({ Component, pageProps }) => {
         <link rel="shortcut icon" href={getStrapiMedia(global.favicon.url)} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,500;0,700;1,300&family=Russo+One&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,500;0,700;0,900;1,300&family=Russo+One&display=swap" rel="stylesheet" />
         <link href="/fonts/javanese.css" rel="stylesheet" />
       </Head>
       {/* Global site metadata */}
