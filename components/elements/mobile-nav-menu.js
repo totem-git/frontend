bb4ca328240cd9a1c2e3f6782d3cabcff1e0d349
Tomás <tomas.benitez.silva@gmail.com>
2021-12-08 11:36:44 -0300

    WIP navbar

--- components/elements/mobile-nav-menu.js
+++ components/elements/mobile-nav-menu.js
@@ -1,46 +1,61 @@
 import PropTypes from "prop-types"
-import { MdClose, MdChevronRight } from "react-icons/md"
 import { mediaPropTypes, linkPropTypes, buttonLinkPropTypes } from "utils/types"
 import { useLockBodyScroll } from "utils/hooks"
 import { getButtonAppearance } from "utils/button"
 import ButtonLink from "./button-link"
-import NextImage from "./image"
+import Image from "next/image"
 import CustomLink from "./custom-link"
+import Link from "next/link"
 
 const MobileNavMenu = ({ navbar, closeSelf }) => {
   // Prevent window scroll while mobile nav menu is open
   useLockBodyScroll()
 
   return (
-    <div className="w-screen h-screen fixed top-0 left-0 overflow-y-scroll bg-white z-10 pb-6">
-      <div className="container h-full flex flex-col justify-between">
+    <div className="fixed inset-0 overflow-y-scroll bg-black text-white pb-6 md:hidden z-50">
+      <div className="container h-full flex flex-col">
         {/* Top section */}
-        <div className="flex flex-row justify-between py-2 items-center">
+        <div className="flex flex-row justify-between h-20 items-center">
           {/* Company logo */}
-          <NextImage width="120" height="33" media={navbar.logo} />
+          <a className="text-primary-600 font-javanese text-2xl h-4">
+            TOTEM RESORT
+          </a>
           {/* Close button */}
           <button onClick={closeSelf} className="py-1 px-1">
-            <MdClose className="h-8 w-auto" />
+            <Image src="/icons/close-menu-icon.svg" width="36" height="36" />
           </button>
         </div>
         {/* Bottom section */}
-        <div className="flex flex-col justify-end w-9/12 mx-auto">
-          <ul className="flex flex-col list-none gap-6 items-baseline text-xl mb-10">
+        <div className="flex flex-col items-center w-9/12 mx-auto mt-auto mb-auto pb-24">
+          <ul className="flex flex-col list-none items-baseline text-2xl mb-10">
             {navbar.links.map((navLink) => (
               <li key={navLink.id} className="block w-full">
                 <CustomLink link={navLink}>
-                  <div className="hover:text-gray-900 py-6 flex flex-row justify-between items-center">
+                  <div className="text-primary-600 hover:text-primary-300 uppercase py-6 text-center">
                     <span>{navLink.text}</span>
-                    <MdChevronRight className="h-8 w-auto" />
                   </div>
                 </CustomLink>
               </li>
             ))}
           </ul>
-          <ButtonLink
+
+          {navbar.button && (<ButtonLink
             button={navbar.button}
-            appearance={getButtonAppearance(navbar.button.type, "light")}
-          />
+            appearance={getButtonAppearance(navbar.button.type, "dark")}
+            size="text-2xl"
+          />)}
+
+          <div className="flex gap-12 mt-32">
+            <a href="#">
+              <Image src="/icons/ig-icon.svg" width="40" height="40" />
+            </a>
+            <a href="#">
+              <Image src="/icons/fb-icon.svg" width="40" height="40" />
+            </a>
+            <a href="#">
+              <Image src="/icons/yt-icon.svg" width="40" height="40" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
