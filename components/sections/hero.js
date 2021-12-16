7b898298c696c61eeb3bfe114cdd3f1c8659c7f1
DesarrolloDMIX <dev@digitalmix.ar>
2021-12-16 17:47:49 +0100

    Change hero to new styles

--- components/sections/hero.js
+++ components/sections/hero.js
@@ -1,37 +1,55 @@
 import Markdown from "react-markdown"
 import { getButtonAppearance } from "utils/button"
 import ButtonLink from "../elements/button-link"
-import NextImage from "../elements/image"
+import Image from "next/image"
 
 const Hero = ({ data }) => {
   return (
-    <main className="container flex flex-col md:flex-row items-center justify-between py-12">
-      {/* Left column for content */}
-      <div className="flex-1 sm:pr-8">
-        {/* Hero section label */}
-        <p className="uppercase tracking-wide font-semibold">{data.label}</p>
-        {/* Big title */}
-        <h1 className="title font-russo mt-2 sm:mt-0 mb-4 sm:mb-2">{data.title}</h1>
-        {/* Description paragraph */}
-        <p className="text-xl mb-6">{data.description}</p>
-        {/* Buttons row */}
-        <div className="flex flex-row flex-wrap gap-4">
-          {data.buttons.map((button) => (
-            <ButtonLink
-              button={button}
-              appearance={getButtonAppearance(button.type, "light")}
-              key={button.id}
-            />
-          ))}
+    <main className="text-white text-center bg-black">
+      <div className="relative py-12 px-16">
+        <div className="absolute inset-0">
+          {/* TODO add poster attribute */}
+          <video playsInline autoPlay muted loop id="bgvid" className="w-full h-full object-cover">
+            <source src="/vid/totem1_trimmed_720_optimized.webm" type="video/webm" />
+            <source src="/vid/totem1_trimmed_720_optimized.mp4" type="video/mp4" />
+          </video>
         </div>
-        {/* Small rich text */}
-        <div className="text-base md:text-sm mt-4 sm:mt-3 rich-text-hero">
-          <Markdown>{data.smallTextWithLink}</Markdown>
+        <div className="absolute inset-0 bg-black opacity-40 m-0"></div>
+        <div className="relative flex flex-col items-center justify-between space-y-8 z-10">
+          <div className="space-y-2">
+            <h3 className="text-2xl">Suitonsectetuer</h3>
+            <h2 className="text-4xl text-primary-600">Lorem ipsum hirtyt moirtere <span className="text-bg-effect">troiew</span></h2>
+          </div>
+          <ButtonLink
+            button={{
+              url: "#",
+              newTab: false,
+              text: "Catch a Fish",
+              type: "primary"
+            }}
+            appearance={getButtonAppearance('primary', 'dark')}
+          />
         </div>
       </div>
-      {/* Right column for the image */}
-      <div className="flex-shrink-0 w-full md:w-6/12 mt-6 md:mt-0">
-        <NextImage media={data.picture} />
+      <div className="flex justify-center divide-x-2 border-white px-4 mt-6">
+        <div className="grow py-2">
+          <a href="#" className="flex flex-col space-y-2">
+            <Image src="/icons/icon-1.svg" width="16" height="16" />
+            <span>The facilities</span>
+          </a>
+        </div>
+        <div className="grow py-2">
+          <a href="#" className="flex flex-col space-y-2">
+            <Image src="/icons/icon-2.svg" width="16" height="16" />
+            <span>The facilities</span>
+          </a>
+        </div>
+        <div className="grow py-2">
+          <a href="#" className="flex flex-col space-y-2">
+            <Image src="/icons/icon-3.svg" width="16" height="16" />
+            <span>The facilities</span>
+          </a>
+        </div>
       </div>
     </main>
   )
