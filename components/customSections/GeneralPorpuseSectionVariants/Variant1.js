a95c90b4827c78d92878d7bf1888910aa45b25ff
DesarrolloDMIX <dev@digitalmix.ar>
2021-12-26 18:47:39 +0100

    Added first variant for general porpuse section

--- components/customSections/GeneralPorpuseSectionVariants/Variant1.js
+++ components/customSections/GeneralPorpuseSectionVariants/Variant1.js
@@ -1,7 +1,25 @@
+import ButtonLink from "@/components/elements/button-link"
+import { getButtonAppearance } from "utils/button"
+
 const Variant1 = ({ data }) => {
     return (
-        <section className="bg-primary-900 py-20 text-center">
-            <h2 className="title text-white">{data.title}</h2>
+        <section className="lg:px-24 py-12">
+            <div className="py-16 bg-black text-light-grey-blue">
+                <div className="px-8 lg:px-24 sm:max-w-xl lg:max-w-4xl mx-auto flex flex-col items-center text-center space-y-4">
+                    <h2 className="text-primary-600 text-3xl font-russo font-thin">{data.title}</h2>
+                    <p>{data.text}</p>
+                    <div className="flex space-x-2 pt-8">
+                        {data.CTAs && data.CTAs.map(cta => (
+                            <ButtonLink
+                                button={cta}
+                                appearance={getButtonAppearance(cta.type, 'dark')}
+                                key={cta.id}
+                            />
+                        ))}
+                    </div>
+                </div>
+                <div className="separator-fish separator-fish h-6 !mt-16"></div>
+            </div>
         </section>
     )
 }
