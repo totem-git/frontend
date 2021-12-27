8a49b2f48c9dd0827e66eb8b1bc4575087120e25
DesarrolloDMIX <dev@digitalmix.ar>
2021-12-27 18:13:58 +0100

    Added variant3 for GPS

--- components/customSections/GeneralPorpuseSection.js
+++ components/customSections/GeneralPorpuseSection.js
@@ -1,9 +1,11 @@
 import Variant1 from "./GeneralPorpuseSectionVariants/Variant1"
 import Variant2 from "./GeneralPorpuseSectionVariants/Variant2"
+import Variant3 from "./GeneralPorpuseSectionVariants/Variant3"
 
 const variants = {
     variant_1: Variant1,
     variant_2: Variant2,
+    variant_3: Variant3,
 }
 
 const GeneralPorpuseSection = ({ data }) => {
---
+++ components/customSections/GeneralPorpuseSectionVariants/Variant3.js
@@ -0,0 +1,28 @@
+import Image from "next/image"
+
+const Variant3 = ({ data }) => {
+    return (
+        <section className="lg:px-24 bg-black">
+            <div className="container max-w-4xl py-16 flex flex-col items-center text-center space-y-4 lg:space-y-12 text-light-grey-blue">
+                <div className="hidden lg:flex w-full flex-col items-center gap-8 lg:flex-row pt-8">
+                    <div className="grow shrink-0 w-3/6 -scale-y-100 -translate-x-2/4">
+                        <Image src="/icons/figure1.svg" width="491" height="128" />
+                    </div>
+                    <div className="separator-fish h-8 shrink-0 grow w-3/6"></div>
+                </div>
+                <div className="space-y-4">
+                    <h4 className="text-primary-600 text-4xl font-russo font-thin">{data.title}</h4>
+                    <p>{data.text}</p>
+                </div>
+                <div className="flex w-full flex-col items-center gap-8 lg:flex-row pt-8">
+                    <div className="separator-fish w-3/6 h-8 grow shrink-0"></div>
+                    <div className="grow shrink-0 w-3/6">
+                        <Image src="/icons/figure1.svg" width="491" height="128" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Variant3
