e89b8d79cc4ae0d89ac27ff65e4371b9613f0e7f
DesarrolloDMIX <dev@digitalmix.ar>
2021-12-27 16:17:00 +0100

    Added variant 2 for GPS

--- components/customSections/GeneralPorpuseSectionVariants/Variant2.js
+++ components/customSections/GeneralPorpuseSectionVariants/Variant2.js
@@ -1,7 +1,32 @@
+import ButtonLink from "@/components/elements/button-link"
+import Image from "next/image"
+import { getButtonAppearance } from "utils/button"
+import { getStrapiMedia } from "utils/media"
+
 const Variant2 = ({ data }) => {
     return (
-        <section className="bg-primary-700 py-20 text-center">
-            <h2 className="title text-white">{data.title}</h2>
+        <section className="pt-20 text-center">
+            <div className="container max-w-4xl space-y-6 px-20">
+                <h4 className="text-4xl font-russo">{data.title}</h4>
+                <p>{data.text}</p>
+                <div className="sm:w-full lg:w-auto inline-block">
+                    <ButtonLink
+                        button={data.CTAs[0]}
+                        appearance={getButtonAppearance(data.CTAs[0].type, 'light')}
+                        wFull
+                    />
+                </div>
+            </div>
+            <div className="lg:mx-24 relative h-[400px] !mt-16">
+                <Image src={getStrapiMedia(data.media[0].url)} objectFit="cover" layout="fill" />
+                <span
+                    style={{
+                        backgroundImage: "radial-gradient(rgba(255, 255, 255, .4), transparent 25%)",
+                        backgroundSize: "14px 10px"
+                    }}
+                    className="absolute top-1/3 left-1/4 right-0 bottom-10"
+                ></span>
+            </div>
         </section>
     )
 }
