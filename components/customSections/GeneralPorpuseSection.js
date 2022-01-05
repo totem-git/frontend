4c4da6b57564e9e36f23a1efbf22d8377e6a9214
DesarrolloDMIX <dev@digitalmix.ar>
2022-01-05 16:10:33 +0100

    Added variant 4 reverse for GPS

--- components/customSections/GeneralPorpuseSection.js
+++ components/customSections/GeneralPorpuseSection.js
@@ -2,12 +2,14 @@ import Variant1 from "./GeneralPorpuseSectionVariants/Variant1"
 import Variant2 from "./GeneralPorpuseSectionVariants/Variant2"
 import Variant3 from "./GeneralPorpuseSectionVariants/Variant3"
 import Variant4 from "./GeneralPorpuseSectionVariants/Variant4"
+import Variant4_Reverse from "./GeneralPorpuseSectionVariants/Variant4_Reverse"
 
 const variants = {
     variant_1: Variant1,
     variant_2: Variant2,
     variant_3: Variant3,
     variant_4: Variant4,
+    variant_4_reverse: Variant4_Reverse,
 }
 
 const GeneralPorpuseSection = ({ data }) => {
