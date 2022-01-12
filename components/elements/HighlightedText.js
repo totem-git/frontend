95096148af2d4f47c764cb3aebd7779396daa8cc
DesarrolloDMIX <dev@digitalmix.ar>
2022-01-12 19:08:01 +0100

    Added component that highlights text dinamically
    The text passed to this component can be formatted to indicate highlighted parts

---
+++ components/elements/HighlightedText.js
@@ -0,0 +1,25 @@
+const HighlightedText = ({
+    text,
+    tag,
+    className = '',
+    highlightClasses = ['text-highlight before:bg-primary-600 text-white']
+}) => {
+    const Tag = tag
+    let textArray = text.split('^')
+
+    return (
+        <Tag className={className}>
+            {textArray.map((text, i) => {
+                if (text == '') return ''
+                if (i % 2 == 0) return <span key={i} className="z-10 relative">{text}</span>
+
+                let classesIndex = parseInt(text.slice(0, 1))
+                text = text.slice(2)
+                let classes = highlightClasses[classesIndex] ?? highlightClasses[0]
+                return <span key={i} className={classes}>{text}</span>
+            })}
+        </Tag>
+    )
+}
+
+export default HighlightedText
