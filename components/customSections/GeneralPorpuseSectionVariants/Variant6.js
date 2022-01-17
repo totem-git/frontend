95d7cb1669c9f1a59f67a114fafb5c55699df515
DesarrolloDMIX <dev@digitalmix.ar>
2022-01-17 17:57:55 +0100

    Adjusted according to prototype

--- components/customSections/GeneralPorpuseSectionVariants/Variant6.js
+++ components/customSections/GeneralPorpuseSectionVariants/Variant6.js
@@ -1,25 +1,37 @@
 import Image from "next/image"
 import Markdown from "react-markdown"
 import { getStrapiMedia } from "utils/media"
+import ButtonLink from '@/components/elements/button-link'
+import { getButtonAppearance } from '../../../utils/button'
 
 const Variant6 = ({ data }) => {
     let picture = data.media[0]
 
     return (
-        <section className="py-8 overflow-x-hidden bg-light-grey-blue">
+        <section className="pt-16 pb-8 overflow-x-hidden bg-light-grey-blue">
             <div className="container flex flex-col items-center space-y-4 max-w-none xl:px-16 md:flex-row">
-                <div className="relative flex-1 w-full p-4 max-w-xl">
+                <div className="relative flex-1 w-full p-4 max-w-xl hidden lg:block">
                     <Image src={getStrapiMedia(picture.url)} layout="responsive" objectPosition="center" objectFit="cover" width={picture.width} height={picture.height} />
                     <div className="mt-8">
                         <Image src="/icons/variant6_icon2.svg" width={142} height={17} />
                     </div>
                 </div>
                 <div className="relative flex-1 lg:min-w-[65ch] space-y-4 sm:pl-2 lg:pl-16">
-                    <div className="prose text-gray-700">
+                    <h4 className="text-3xl font-russo font-medium text-center text-gray-600">{data.title}</h4>
+                    <div className="prose text-gray-700 text-center leading-tight px-2">
                         <Markdown>{data.text}</Markdown>
                     </div>
-                    <div className="mt-8">
-                        <Image src="/icons/variant6_icon.svg" width={74} height={13} />
+                    <div className="pt-4 space-y-6">
+                        <Image src={data.extraIcon ? getStrapiMedia(data.extraIcon.url) : '/icons/variant6_icon_1.svg'} width={148} height={26} />
+                        {data.CTAs.length && (
+                            <div className="w-full">
+                                <ButtonLink
+                                    button={data.CTAs[0]}
+                                    appearance={getButtonAppearance(data.CTAs[0].type, 'light')}
+                                    wFull
+                                />
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
