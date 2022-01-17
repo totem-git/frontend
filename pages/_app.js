adceaaa07310ed6516a3a32b5900d37f30bf59b6
DesarrolloDMIX <dev@digitalmix.ar>
2022-01-17 11:57:47 +0100

    corrected wrong attribute value for 'crossOrigin'

--- pages/_app.js
+++ pages/_app.js
@@ -22,7 +22,7 @@ const MyApp = ({ Component, pageProps }) => {
       <Head>
         <link rel="shortcut icon" href={getStrapiMedia(global.favicon.url)} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,300&display=swap" rel="stylesheet" />
         <link href="/fonts/russo.css" rel="stylesheet" />
       </Head>
