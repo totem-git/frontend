585a6dd4015eb56c3b836c0eeb8767368ab96f5e
DesarrolloDMIX <dev@digitalmix.ar>
2022-01-19 18:14:51 +0100

    Removed the absolute positioning from the element

--- components/elements/SliderIndicators.js
+++ components/elements/SliderIndicators.js
@@ -1,6 +1,6 @@
 const SliderIndicators = ({ activeSlides }) => {
     return (
-        <div className="absolute bottom-2 left-1/2 -translate-x-1/2 flex items-center space-x-2">
+        <div className="flex items-center space-x-2">
             {activeSlides.map((isActive, i) => (
                 <div key={i} className={`${isActive ? 'p-1 bg-white' : 'p-0.5 bg-gray-500'} rounded-full transition-all`}></div>
             ))}
