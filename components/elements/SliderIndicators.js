6a076e3fca0e720221e3e029cc14f6150975fc34
DesarrolloDMIX <dev@digitalmix.ar>
2022-01-19 18:30:04 +0100

    Added customizable classNames

--- components/elements/SliderIndicators.js
+++ components/elements/SliderIndicators.js
@@ -1,8 +1,8 @@
-const SliderIndicators = ({ activeSlides }) => {
+const SliderIndicators = ({ activeSlides, activeClassName = 'p-1 bg-white', inactiveClassName = 'p-0.5 bg-gray-500', className = 'rounded-full transition-all' }) => {
     return (
         <div className="flex items-center space-x-2">
             {activeSlides.map((isActive, i) => (
-                <div key={i} className={`${isActive ? 'p-1 bg-white' : 'p-0.5 bg-gray-500'} rounded-full transition-all`}></div>
+                <div key={i} className={`${isActive ? activeClassName : inactiveClassName} ${className}`}></div>
             ))}
         </div>
     )
