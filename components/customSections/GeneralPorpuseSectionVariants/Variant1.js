985a837f2139acdc27dbafd3c6496640c48136ce
DesarrolloDMIX <dev@digitalmix.ar>
2022-01-20 16:04:24 +0100

    adjusted according to proto (mobile)

--- components/customSections/GeneralPorpuseSectionVariants/Variant1.js
+++ components/customSections/GeneralPorpuseSectionVariants/Variant1.js
@@ -1,30 +1,40 @@
 import ButtonLink from "@/components/elements/button-link"
 import { getButtonAppearance } from "utils/button"
 import Markdown from "react-markdown"
+import { getStrapiMedia } from "utils/media"
+import NextImage from "@/components/elements/image"
 
 const Variant1 = ({ data }) => {
     return (
-        <section className="lg:px-24">
-            <div className="py-16 bg-black text-light-grey-blue">
-                <div className="px-8 lg:px-24 sm:max-w-xl lg:max-w-4xl mx-auto flex flex-col lg:items-center lg:text-center space-y-4">
-                    <h2 className="text-primary-600 text-3xl font-russo font-thin">{data.title}</h2>
-                    <div className="prose prose-invert">
-                        <Markdown>{data.text}</Markdown>
-                    </div>
-                    {!!data.CTAs.length && (
-                        <div className="flex space-x-2 pt-8">
-                            {data.CTAs.map(cta => (
-                                <ButtonLink
-                                    button={cta}
-                                    appearance={getButtonAppearance(cta.type, 'dark')}
-                                    key={cta.id}
-                                />
-                            ))}
-                        </div>
-                    )}
+        <section className="py-16 bg-black text-light-grey-blue">
+            {data.media.length > 0 && (
+                <div className="px-16 mb-16 hidden">
+                    <NextImage media={data.media[0]} />
                 </div>
-                <div className="separator-fish separator-fish h-6 !mt-16"></div>
+            )}
+            <div className="flex flex-col items-center text-center space-y-4 px-8 lg:px-24 sm:max-w-xl lg:max-w-4xl mx-auto">
+                <h2 className="text-primary-600 text-3xl font-russo font-thin">{data.title}</h2>
+                <div className="prose prose-invert">
+                    <Markdown>{data.text}</Markdown>
+                </div>
+                {!!data.CTAs.length && (
+                    <div className="flex w-full flex-col space-y-2 pt-8">
+                        {data.CTAs.map(cta => (
+                            <ButtonLink
+                                button={cta}
+                                appearance={getButtonAppearance(cta.type, 'dark')}
+                                key={cta.id}
+                                wFull
+                            />
+                        ))}
+                    </div>
+                )}
             </div>
+            {data.media.length > 0 && (
+                <div className="px-16 mt-16">
+                    <NextImage media={data.media[0]} />
+                </div>
+            )}
         </section>
     )
 }
