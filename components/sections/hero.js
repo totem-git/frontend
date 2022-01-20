0ad85412c9222081a18933c6ba16635bec43fa41
DesarrolloDMIX <dev@digitalmix.ar>
2022-01-20 12:51:15 +0100

    added white version for link icons

--- components/sections/hero.js
+++ components/sections/hero.js
@@ -3,9 +3,23 @@ import ButtonLink from "../elements/button-link"
 import Image from "next/image"
 import { getStrapiMedia } from "utils/media"
 import HighlightedText from "../elements/HighlightedText"
+import { useEffect, useRef, useState } from "react"
 
 const Hero = ({ data }) => {
-  const isLanding = data.type === 'landing'
+  const isLanding = data.type === 'landing' || data.type === 'home'
+  const isHome = data.type === 'home'
+  const navRef = useRef()
+  const [currentUrl, setCurrentUrl] = useState('')
+
+  useEffect(() => {
+    let url = window.location.pathname
+    if (url.indexOf('?') != -1) url = url.slice(0, url.indexOf('?'))
+    if (url.indexOf('#') != -1) url = url.slice(0, url.indexOf('#'))
+
+    setCurrentUrl(url)
+  }, [])
+
+  console.log(currentUrl);
 
   return (
     <main className="text-white text-center bg-black">
@@ -23,12 +37,14 @@ const Hero = ({ data }) => {
         </div>
         <div className="absolute inset-0 bg-black opacity-60 m-0"></div>
         <div className="relative flex flex-col items-center justify-between space-y-16 z-10 w-full">
-          <div className="space-y-4 w-full flex flex-col items-center">
-            <HighlightedText tag="h2" className="text-4xl text-primary-600 font-basker" highlightClasses={['text-bg-effect']} text={data.title} />
-            <h3 className="text-3xl font-russo max-w-xs">{data.label}</h3>
-            <div className="max-w-xs px-16">
-              <Image src="/icons/stars.svg" width={500} height={100} />
-            </div>
+          <div className="space-y-5 w-full flex flex-col items-center">
+            <HighlightedText tag="h2" className={`text-4xl text-primary-600 ${isHome ? 'font-basker' : 'font-russo'}`} highlightClasses={['text-bg-effect']} text={data.title} />
+            <h3 className={`${isHome ? 'text-3xl' : 'text-xl'} font-russo max-w-xs`}>{data.label}</h3>
+            {isHome && (
+              <div className="max-w-xs px-16">
+                <Image src="/icons/stars.svg" width={500} height={100} />
+              </div>
+            )}
             <div className="w-full px-6">
               <ButtonLink
                 wFull
@@ -43,23 +59,23 @@ const Hero = ({ data }) => {
         </div>
       </div>
       {isLanding &&
-        <div className="flex justify-center divide-x-2 border-white">
+        <div ref={navRef} className="flex justify-center divide-x-2 border-white">
           <div className="grow pb-4 pt-6">
-            <a href="#" className="flex flex-col space-y-2 group">
-              <Image src="/icons/icon-1.svg" width="16" height="16" />
-              <span className="w-max inline-block mx-auto border-b-4 border-transparent group-hover:border-white transition">Lodging</span>
+            <a href="/the-facilities" className="flex flex-col space-y-2 group">
+              {(currentUrl == '/the-facilities') ? <Image src="/icons/icon-1-white.svg" width="16" height="16" /> : <Image src="/icons/icon-1.svg" width="16" height="16" />}
+              <span className={`w-max inline-block mx-auto border-b-4 ${(currentUrl == '/the-facilities') ? 'border-white' : 'border-transparent'} group-hover:border-white transition`}>Lodging</span>
             </a>
           </div>
           <div className="grow pb-4 w-[36%] pt-6">
             <a href="#" className="flex flex-col space-y-2 group">
-              <Image src="/icons/icon-2.svg" width="16" height="16" />
-              <span className="w-max inline-block mx-auto border-b-4 border-transparent group-hover:border-white transition">Fishing experience</span>
+              {(currentUrl == '#') ? <Image src="/icons/icon-2-white.svg" width="16" height="16" /> : <Image src="/icons/icon-2.svg" width="16" height="16" />}
+              <span className={`w-max inline-block mx-auto border-b-4 ${(currentUrl == '#') ? 'border-white' : 'border-transparent'} group-hover:border-white transition`}>Fishing experience</span>
             </a>
           </div>
           <div className="grow pb-4 pt-6">
             <a href="#" className="flex flex-col space-y-2 group">
-              <Image src="/icons/icon-3.svg" width="16" height="16" />
-              <span className="w-max inline-block mx-auto border-b-4 border-transparent group-hover:border-white transition">Events</span>
+              {(currentUrl == '#') ? <Image src="/icons/icon-3-white.svg" width="16" height="16" /> : <Image src="/icons/icon-3.svg" width="16" height="16" />}
+              <span className={`w-max inline-block mx-auto border-b-4 ${(currentUrl == '#') ? 'border-white' : 'border-transparent'} group-hover:border-white transition`}>Events</span>
             </a>
           </div>
         </div>
