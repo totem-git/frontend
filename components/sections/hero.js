6e213116d2c9d25637b9295b8ca2163437699bb9
DesarrolloDMIX <dev@digitalmix.ar>
2022-01-21 23:05:29 +0100

    Added optional extraIcon and made CTA optional

--- components/sections/hero.js
+++ components/sections/hero.js
@@ -4,6 +4,7 @@ import Image from "next/image"
 import { getStrapiMedia } from "utils/media"
 import HighlightedText from "../elements/HighlightedText"
 import { useEffect, useRef, useState } from "react"
+import NextImage from "../elements/image"
 
 const Hero = ({ data }) => {
   const isLanding = data.type === 'landing' || data.type === 'home'
@@ -37,11 +38,9 @@ const Hero = ({ data }) => {
     setCurrentUrl(url)
   }, [])
 
-  console.log(currentUrl);
-
   return (
-    <main className="text-white text-center bg-black">
-      <div className={`relative flex justify-center items-center py-12 lg:pb-4 sm:py-32 ${isLanding && 'h-[85vh]'}`}>
+    <main className="text-white text-center bg-black h-screen flex flex-col">
+      <div className={`relative flex justify-center items-center py-12 lg:pb-4 sm:py-32 grow`}>
         <div className="absolute inset-0">
           {data.picture.mime.startsWith('image') &&
             <Image src={getStrapiMedia(data.picture.url)} objectFit="cover" layout="fill" />
@@ -55,21 +54,28 @@ const Hero = ({ data }) => {
         </div>
         <div className="absolute inset-0 bg-black opacity-60 m-0"></div>
         <div className="relative flex flex-col items-center justify-between space-y-16 z-10 w-full">
-          <div className="space-y-5 w-full flex flex-col items-center">
+          <div className="space-y-5 w-full flex flex-col items-center px-4">
+            {data.extraIcon && (
+              <div className="w-40">
+                <NextImage media={data.extraIcon} />
+              </div>
+            )}
             <HighlightedText tag="h2" className={`text-4xl text-primary-600 ${isHome ? 'font-basker' : 'font-russo'}`} highlightClasses={['text-bg-effect']} text={data.title} />
-            <h3 className={`${isHome ? 'text-3xl' : 'text-xl'} font-russo max-w-xs`}>{data.label}</h3>
+            <h3 className={`${isHome ? 'text-3xl' : 'text-xl'} font-russo`}>{data.label}</h3>
             {isHome && (
               <div className="max-w-xs px-16">
                 <Image src="/icons/stars.svg" width={500} height={100} />
               </div>
             )}
-            <div className="w-full px-6">
-              <ButtonLink
-                wFull
-                button={data.buttons[0]}
-                appearance={getButtonAppearance(data.buttons[0].type, 'dark')}
-              />
-            </div>
+            {!!data.buttons.length && (
+              <div className="w-full px-6">
+                <ButtonLink
+                  wFull
+                  button={data.buttons[0]}
+                  appearance={getButtonAppearance(data.buttons[0].type, 'dark')}
+                />
+              </div>
+            )}
           </div>
           <div className="hidden lg:block">
             <Image src="/icons/arrow-down.svg" width={32} height={32} />
@@ -79,7 +85,7 @@ const Hero = ({ data }) => {
       {isLanding &&
         <div ref={navRef} className="flex justify-center divide-x-2 border-white">
           {navLinks.map((link, i) => (
-            <div className={`grow pb-4 ${i == 1 && 'w-[36%]'} pt-6`}>
+            <div key={i} className={`grow pb-4 ${i == 1 && 'w-[36%]'} pt-6`}>
               <a href={link.url} className="flex flex-col space-y-2 group">
                 {(currentUrl == link.url) ? <Image src={`/icons/${link.icon}-white.svg`} width="16" height="16" /> : <Image src={`/icons/${link.icon}.svg`} width="16" height="16" />}
                 <span className={`w-max inline-block mx-auto border-b-4 ${(currentUrl == link.url) ? 'border-white' : 'border-transparent'} group-hover:border-white transition`}>{link.text}</span>
