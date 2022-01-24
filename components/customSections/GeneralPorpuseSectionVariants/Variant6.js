679b415e3c47e7fd24fb19d6781007491b7f3dad
DesarrolloDMIX <dev@digitalmix.ar>
2022-01-24 12:40:39 +0100

    Adjusted according to proto (mobile)

--- components/customSections/GeneralPorpuseSectionVariants/Variant6.js
+++ components/customSections/GeneralPorpuseSectionVariants/Variant6.js
@@ -1,37 +1,26 @@
 import Image from "next/image"
 import Markdown from "react-markdown"
 import { getStrapiMedia } from "utils/media"
-import ButtonLink from '@/components/elements/button-link'
-import { getButtonAppearance } from '../../../utils/button'
 
 const Variant6 = ({ data }) => {
     let picture = data.media[0]
 
     return (
-        <section className="pt-16 pb-8 overflow-x-hidden bg-light-grey-blue">
-            <div className="container flex flex-col items-center space-y-4 max-w-none xl:px-16 md:flex-row">
-                <div className="relative flex-1 w-full p-4 max-w-xl hidden lg:block">
+        <section className="py-8 overflow-x-hidden bg-light-grey-blue">
+            {data.title && (<h4 className="text-center text-gray-600 font-russo text-2xl mb-6">{data.title}</h4>)}
+            <div className="flex flex-col items-center space-y-4 max-w-none xl:px-16 md:flex-row">
+                <div className="relative flex-1 w-full max-w-xl">
                     <Image src={getStrapiMedia(picture.url)} layout="responsive" objectPosition="center" objectFit="cover" width={picture.width} height={picture.height} />
                     <div className="mt-8">
-                        <Image src="/icons/variant6_icon2.svg" width={142} height={17} />
+                        <Image src="/icons/variant6_icon2.svg" objectPosition="center" objectFit="cover" width={142} height={17} />
                     </div>
                 </div>
-                <div className="relative flex-1 lg:min-w-[65ch] space-y-4 sm:pl-2 lg:pl-16">
-                    <h4 className="text-3xl font-russo font-medium text-center text-gray-600">{data.title}</h4>
-                    <div className="prose text-gray-700 text-center leading-tight px-2">
+                <div className="container relative flex-1 lg:min-w-[65ch] space-y-4 sm:pl-2 lg:pl-16">
+                    <div className="prose text-gray-700">
                         <Markdown>{data.text}</Markdown>
                     </div>
-                    <div className="pt-4 space-y-6">
+                    <div className="mt-8">
                         <Image src={data.extraIcon ? getStrapiMedia(data.extraIcon.url) : '/icons/variant6_icon_1.svg'} width={148} height={26} />
-                        {data.CTAs.length > 0 && (
-                            <div className="w-full">
-                                <ButtonLink
-                                    button={data.CTAs[0]}
-                                    appearance={getButtonAppearance(data.CTAs[0].type, 'light')}
-                                    wFull
-                                />
-                            </div>
-                        )}
                     </div>
                 </div>
             </div>
--- components/customSections/GeneralPorpuseSectionVariants/Variant6_Reverse.js
+++ components/customSections/GeneralPorpuseSectionVariants/Variant6_Reverse.js
@@ -7,19 +7,19 @@ const Variant6_Reverse = ({ data }) => {
 
     return (
         <section className="py-8 overflow-x-hidden bg-light-grey-blue">
-            <div className="container flex flex-col items-center space-y-4 max-w-none xl:px-16 md:flex-row-reverse">
-                <div className="relative flex-1 w-full p-4 max-w-xl">
+            <div className="flex flex-col items-center space-y-4 max-w-none xl:px-16 md:flex-row-reverse">
+                <div className="relative flex-1 w-full max-w-xl">
                     <Image src={getStrapiMedia(picture.url)} layout="responsive" width={picture.width} height={picture.height} />
                     <div className="mt-8">
                         <Image src="/icons/variant6_icon2.svg" objectPosition="center" objectFit="cover" width={142} height={17} />
                     </div>
                 </div>
-                <div className="relative lg:min-w-[65ch] mt-auto flex-1 space-y-4 sm:pl-2 lg:pr-16">
+                <div className="container relative lg:min-w-[65ch] mt-auto flex-1 space-y-4 sm:pl-2 lg:pr-16">
                     <div className="prose text-gray-700">
                         <Markdown>{data.text}</Markdown>
                     </div>
                     <div className="mt-8">
-                        <Image src="/icons/variant6_icon.svg" width={74} height={13} />
+                        <Image src={data.extraIcon ? getStrapiMedia(data.extraIcon.url) : '/icons/variant6_icon_1.svg'} width={148} height={26} />
                     </div>
                 </div>
             </div>
