18a69e2d668b124cc5f6388c2159efd05b5f58c9
DesarrolloDMIX <dev@digitalmix.ar>
2022-01-27 10:16:31 +0100

    Changes in the structure of env variables

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
+let imageHosts = process.env.IMAGE_HOSTS || ''
+imageHosts = imageHosts.split(', ')
+
 module.exports = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
   images: {
-    domains: ["ec2-3-128-207-161.us-east-2.compute.amazonaws.com", "localhost"]
+    domains: ["ec2-3-128-207-161.us-east-2.compute.amazonaws.com", "localhost"].concat(imageHosts)
   },
 }
