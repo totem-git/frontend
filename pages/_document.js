5db09b90c671591c63ebb2435808f17ea57fb7d7
Tomás <tomas.benitez.silva@gmail.com>
2022-03-31 13:44:27 -0300

    pulled changes made on live site

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head />
         <body>
+          <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MKSQ7M5" height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
           <Main />
           <NextScript />
         </body>
