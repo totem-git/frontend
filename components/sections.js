e31f791bec6f36cd86a2932791533d63c48d4655
Tomás <tomas.benitez.silva@gmail.com>
2022-06-15 15:29:40 -0300

    Added ID to contentSections container

--- components/sections.js
+++ components/sections.js
@@ -81,7 +81,7 @@ const PreviewModeBanner = () => {
 // Display the list of sections
 const Sections = ({ sections, preview }) => {
   return (
-    <div className="flex flex-col">
+    <div id="contentSections" className="flex flex-col">
       {/* Show a banner if preview mode is on */}
       {preview && <PreviewModeBanner />}
       {/* Show the actual sections */}
