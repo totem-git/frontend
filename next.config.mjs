d3080ea21a9c230eea373ec51a3ec4ada58fed3e
Tomás <tomas.benitez.silva@gmail.com>
2022-08-23 12:20:44 -0300

    implemented rewrites for article pages

---
+++ next.config.mjs
@@ -0,0 +1,28 @@
+import fetch from "node-fetch";
+
+let imageHosts = process.env.IMAGE_HOSTS || "";
+imageHosts = imageHosts.split(", ");
+
+export default {
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  images: {
+    domains: [
+      "ec2-3-128-207-161.us-east-2.compute.amazonaws.com",
+      "localhost",
+      "totemresorts.com",
+    ].concat(imageHosts),
+  },
+  async rewrites() {
+    const articleCategories = await fetch(
+      `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/pages/list-article-categories`
+    ).then((res) => res.json());
+
+    return articleCategories.map((category) => ({
+      source: `/${category}/:slug`,
+      destination: `/article/:slug?category=${category}`,
+    }));
+  },
+};
