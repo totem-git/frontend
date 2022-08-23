5102ed6a1294cc3e0f3de005c96c69f11f8c275b
Tomás <tomas.benitez.silva@gmail.com>
2022-08-23 12:26:13 -0300

    feature: add custom html through strapi rich text

--- components/sections/rich-text.js
+++ components/sections/rich-text.js
@@ -4,7 +4,19 @@ import Markdown from "react-markdown";
 const RichText = ({ data }) => {
   return (
     <div className="container prose prose-lg py-12">
-      <Markdown>{data.content}</Markdown>
+      <Markdown
+        components={{
+          pre: ({ children }) => (
+            <div
+              dangerouslySetInnerHTML={{
+                __html: children[0].props.children[0],
+              }}
+            />
+          ),
+        }}
+      >
+        {data.content}
+      </Markdown>
     </div>
   );
 };
