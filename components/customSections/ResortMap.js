d28d9117f3e963bf58c24da3d7a98eb1b282f3c7
Tomás <tomas.benitez.silva@gmail.com>
2023-08-08 13:55:21 -0300

    fix resort maps descriptions

--- components/customSections/ResortMap.js
+++ components/customSections/ResortMap.js
@@ -46,9 +46,9 @@ const ResortMap = ({ data, prependBreadcrumbs }) => {
           </p>
           <div
             ref={imageContainerRef}
-            className="mx-auto max-w-6xl justify-center lg:flex"
+            className="mx-auto max-w-7xl justify-center lg:flex"
           >
-            <div className="shrink items-start gap-4 sm:flex sm:bg-light-grey-blue sm:p-8 lg:w-full lg:flex-col lg:bg-transparent">
+            <div className="shrink items-start gap-4 sm:flex sm:bg-light-grey-blue sm:p-4 lg:w-full lg:flex-col lg:bg-transparent">
               <div className="mt-8 overflow-hidden sm:mt-0 sm:w-1/2 lg:w-full">
                 <Image
                   data-original="/imgs/totem-map-numbers-logo.jpg"
@@ -64,58 +64,83 @@ const ResortMap = ({ data, prependBreadcrumbs }) => {
                 <h6 className="mt-6 font-russo text-lg text-primary-600 sm:mt-0">
                   TOTEM LODGE
                 </h6>
-                <p className="mt-4 text-gray-700">
-                  1. Main Lodge: Dining Room and Lounge
-                  <br />
-                  2. Executive Lodge: 5 bedrooms/3 baths
-                  <br />
-                  3. Sunset: 3 bedrooms/2 baths
-                  <br />
-                  4. Leonard: 1 bedroom/1 bath
-                  <br />
-                  5. Hilltop: 2 bedrooms/2 baths
-                  <br />
-                  6. Little Joe: 1 bedroom/1 bath
-                  <br />
-                  7. Robin’s Roost: 2 bedrooms/2 baths
-                  <br />
-                  8. Bear’s Nest: 3 bedrooms/2 baths
-                  <br />
-                  9. Boathouse
-                  <br />
-                  10. Office
-                  <br />
-                  11. Aggie: 3 bedrooms/2 baths
-                  <br />
-                  12. Curly: 4 bedrooms/3 baths
-                  <br />
-                  13. Chalet 1: 1 bedroom/1 bath
-                  <br />
-                  14. Chalet 2: 1 bedroom/1 bath
-                  <br />
-                  15. Stewart: 3 bedrooms/2 baths
-                  <br />
-                  16. Flicker’s: 2 bedrooms/2 baths
-                  <br />
-                  17. Dinty: 2 bedrooms/2 baths
-                  <br />
-                  18. Paradise Point: 4 bedrooms/2 baths
-                  <br />
-                  19. Eagle’s Nest: 3 bedrooms/2 baths
-                  <br />
-                  20. Lookout Bay: 4 bedrooms/3 baths
-                  <br />
-                  21. Midnight Cove: 6 bedrooms/7 baths
-                  <br />
-                  22. Crow’s Nest: 3 bedrooms/2 baths
-                  <br />
-                  23. Willmarth’s: 3 bedrooms/2 baths
-                  <br />
-                  24. Wolf’s Den: GM Quarters
-                </p>
+                <ul>
+                  <li>
+                    1. <b>Main Lodge</b>: Dining Room and Lounge
+                  </li>
+                  <li>
+                    2. <b>Executive Lodge</b>: 5 bedrooms/3 baths
+                  </li>
+                  <li>
+                    3. <b>Sunset</b>: 3 bedrooms/2 baths
+                  </li>
+                  <li>
+                    4. <b>Leonard</b>: 1 bedroom/1 bath
+                  </li>
+                  <li>
+                    5. <b>Hilltop</b>: 2 bedrooms/2 baths
+                  </li>
+                  <li>
+                    6. <b>Little Joe</b>: 1 bedroom/1 bath
+                  </li>
+                  <li>
+                    7. <b>Robin’s Roost</b>: 2 bedrooms/2 baths
+                  </li>
+                  <li>
+                    8. <b>Bear’s Nest</b>: 3 bedrooms/2 baths
+                  </li>
+                  <li>
+                    9. <b>Boathouse</b>
+                  </li>
+                  <li>
+                    10. <b>Office</b>
+                  </li>
+                  <li>
+                    11. <b>Aggie</b>: 3 bedrooms/2 baths
+                  </li>
+                  <li>
+                    12. <b>Curly</b>: 4 bedrooms/3 baths
+                  </li>
+                  <li>
+                    13. <b>Chalet 1</b>: 1 bedroom/1 bath
+                  </li>
+                  <li>
+                    14. <b>Chalet 2</b>: 1 bedroom/1 bath
+                  </li>
+                  <li>
+                    15. <b>Stewart</b>: 3 bedrooms/2 baths
+                  </li>
+                  <li>
+                    16. <b>Flicker’s</b>: 2 bedrooms/2 baths
+                  </li>
+                  <li>
+                    17. <b>Dinty</b>: 2 bedrooms/2 baths
+                  </li>
+                  <li>
+                    18. <b>Paradise Point</b>: 4 bedrooms/2 baths
+                  </li>
+                  <li>
+                    19. <b>Eagle’s Nest</b>: 3 bedrooms/2 baths
+                  </li>
+                  <li>
+                    20. <b>Lookout Bay</b>: 4 bedrooms/3 baths
+                  </li>
+                  <li>
+                    21. <b>Midnight Cove</b>: 6 bedrooms/7 baths
+                  </li>
+                  <li>
+                    22. <b>Crow’s Nest</b>: 3 bedrooms/2 baths
+                  </li>
+                  <li>
+                    23. <b>Willmarth’s</b>: 3 bedrooms/2 baths
+                  </li>
+                  <li>
+                    24. <b>Wolf’s Den</b>: GM Quarters
+                  </li>
+                </ul>
               </div>
             </div>
-            <div className="shrink items-start gap-4 sm:mt-8 sm:flex sm:bg-light-grey-blue sm:p-8 lg:mt-0 lg:w-full lg:flex-col lg:bg-transparent">
+            <div className="shrink items-start gap-4 sm:mt-8 sm:flex sm:bg-light-grey-blue sm:p-4 lg:mt-0 lg:w-full lg:flex-col lg:bg-transparent">
               <div className="mt-8  overflow-hidden sm:mt-0 sm:w-1/2 lg:w-full">
                 <Image
                   data-original="/imgs/yellowbird-map-numbers-logos.jpg"
@@ -131,23 +156,30 @@ const ResortMap = ({ data, prependBreadcrumbs }) => {
                 <h6 className="mt-6 font-russo text-lg text-primary-600 sm:mt-0">
                   YELLOWBIRD LODGE
                 </h6>
-                <p className="mt-4 text-gray-700">
-                  1. Lodge: 10 bedrooms/13 baths
-                  <br />
-                  2. Chalet: 6 bedrooms/8 baths
-                  <br />
-                  3. Hunter Cabin: 3 bedrooms/2 baths
-                  <br />
-                  4. Trapper Cabin: 4 bedrooms/2 baths
-                  <br />
-                  5. Fisher Cabin: 3 bedrooms/2 baths
-                  <br />
-                  6. Angler Cabin: 3 bedrooms/2 baths
-                </p>
+                <ul>
+                  <li>
+                    1. <b>Lodge</b>: 10 bedrooms/13 baths
+                  </li>
+                  <li>
+                    2. <b>Chalet</b>: 6 bedrooms/8 baths
+                  </li>
+                  <li>
+                    3. <b>Hunter Cabin</b>: 3 bedrooms/2 baths
+                  </li>
+                  <li>
+                    4. <b>Trapper Cabin</b>: 4 bedrooms/2 baths
+                  </li>
+                  <li>
+                    5. <b>Fisher Cabin</b>: 3 bedrooms/2 baths
+                  </li>
+                  <li>
+                    6. <b>Angler Cabin</b>: 3 bedrooms/2 baths
+                  </li>
+                </ul>
                 <h6 className="mt-6 font-russo text-lg text-primary-600">
                   PRIVATE ISLANDS OF LOTW
                 </h6>
-                <p className="mt-4 text-gray-700">
+                {/* <p className="mt-4 text-gray-700">
                   25. Island 1: 2 bedrooms/ 1.5 bath
                   <br />
                   26. Island 2 (Cabin #1): 2 bedroom/ 2 baths
@@ -155,10 +187,24 @@ const ResortMap = ({ data, prependBreadcrumbs }) => {
                   27. Island 2 (Cabin #2): 2 bedroom/ 2 baths
                   <br />
                   28. Island 3: 2 bedrooms/ 2 baths
-                </p>
+                </p> */}
+                <ul>
+                  <li>
+                    25. <b>Island 1</b>: 2 bedrooms/ 1.5 bath
+                  </li>
+                  <li>
+                    26. <b>Island 2 (Cabin #1)</b>: 2 bedroom/ 2 baths
+                  </li>
+                  <li>
+                    27. <b>Island 2 (Cabin #2)</b>: 2 bedroom/ 2 baths
+                  </li>
+                  <li>
+                    28. <b>Island 3</b>: 2 bedrooms/ 2 baths
+                  </li>
+                </ul>
               </div>
             </div>
-            <div className="shrink items-start gap-4 sm:mt-8 sm:flex sm:bg-light-grey-blue sm:p-8 lg:mt-0 lg:w-full lg:flex-col lg:bg-transparent">
+            <div className="shrink items-start gap-4 sm:mt-8 sm:flex sm:bg-light-grey-blue sm:p-4 lg:mt-0 lg:w-full lg:flex-col lg:bg-transparent">
               <div className="mt-8 overflow-hidden sm:mt-0 sm:w-1/2 lg:w-full">
                 <Image
                   onClick={() => openViewer(2)}
@@ -174,51 +220,88 @@ const ResortMap = ({ data, prependBreadcrumbs }) => {
                 <h6 className="mt-6 font-russo text-lg text-primary-600 sm:mt-0">
                   WILEY POINT LODGE
                 </h6>
-                <p className="mt-4 text-gray-700">
-                  1. Main Lodge:
-                  <br />
-                  Top level
-                  <br />
-                  Main Level - Dining Room, Bar, Store, Office
-                  <br />
-                  Middle Level - Guest Suites (Rooms 1-8),2nd Bar,
-                  <br />
-                  Games Room,Bottom Level - Gym, Sauna,
-                  <br />
-                  Changerooms & Bathroom
-                  <br />
-                  2. Wolf: 2 big bedrooms/4 beds each/2 baths
-                  <br />
-                  3. Deer: 2 big bedrooms/4 beds each/2 baths
-                  <br />
-                  4. Bear: 2 big bedrooms/4 beds each/2 baths
-                  <br />
-                  5. Fox: 2 big bedrooms/4 beds each/2 baths
-                  <br />
-                  6. Boathouse/Fish Cleaning House: 2 baths
-                  <br />
-                  7. Moose: M1-4 beds, M2-4 beds, M3-2 beds,
-                  <br />
-                  M4-4 beds, M5-4 beds, 1 bath ea.
-                  <br />
-                  8. Little Moose: M6-4 beds, M7-4 beds,
-                  <br />
-                  M8-4 beds, M9-4 beds, 1 bath each
-                  <br />
-                  9. Staff Quarters & Laundry
-                  <br />
-                  10. Elk: E1-4 beds, E2-4 beds, E3-4 beds,
-                  <br />
-                  E4-4 beds, E5-4 beds, M6-4 beds, m7-4 beds,
-                  <br />
-                  M8-4 beds, M9-4 beds, 1 bath each
-                  <br />
-                  Staff Quarters & Laundry
-                  <br />
-                  Elk: E1-4 beds, E2-4 beds, E3-4 beds, E4-4 beds,
-                  <br />
-                  E5-4 beds, 1 bath ea.
-                </p>
+                <ul>
+                  <li>
+                    1. <b>Main Lodge:</b>
+                    <ul>
+                      <li>
+                        <b>Top level</b>
+                      </li>
+                      <li>
+                        <b>Main Level</b> -{" "}
+                        <span className="whitespace-nowrap">Dining Room</span>,
+                        Bar, Store, Office.
+                      </li>
+                      <li>
+                        <b>Middle Level</b> -{" "}
+                        <span className="whitespace-nowrap">
+                          Guest Suites (Rooms 1-8),
+                        </span>{" "}
+                        <span className="whitespace-nowrap">2nd Bar,</span>{" "}
+                        <span className="whitespace-nowrap">Games Room.</span>
+                      </li>
+                      <li>
+                        <b>Bottom Level</b> - Gym, Sauna,{" "}
+                        <span className="whitespace-nowrap">
+                          Changerooms & Bathroom.
+                        </span>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    2. <b>Wolf:</b>{" "}
+                    <span className="whitespace-nowrap">
+                      2 big bedrooms/4 beds each/2 baths
+                    </span>
+                  </li>
+                  <li>
+                    3. <b>Deer:</b>{" "}
+                    <span className="whitespace-nowrap">
+                      2 big bedrooms/4 beds each/2 baths
+                    </span>
+                  </li>
+                  <li>
+                    4. <b>Bear:</b>{" "}
+                    <span className="whitespace-nowrap">
+                      2 big bedrooms/4 beds each/2 baths
+                    </span>
+                  </li>
+                  <li>
+                    5. <b>Fox:</b>{" "}
+                    <span className="whitespace-nowrap">
+                      2 big bedrooms/4 beds each/2 baths
+                    </span>
+                  </li>
+                  <li>
+                    6. <b>Boathouse/Fish Cleaning House:</b>{" "}
+                    <span className="whitespace-nowrap">2 baths</span>
+                  </li>
+                  <li>
+                    7. <b>Moose:</b>{" "}
+                    <span className="whitespace-nowrap">M1-4 beds,</span>{" "}
+                    <span className="whitespace-nowrap">M2-4 beds,</span>{" "}
+                    <span className="whitespace-nowrap">M3-2 beds,</span>{" "}
+                    <span className="whitespace-nowrap">M4-4 beds,</span>{" "}
+                    <span className="whitespace-nowrap">M5-4 beds,</span>{" "}
+                    <span className="whitespace-nowrap">M6-4 beds,</span>{" "}
+                    <span className="whitespace-nowrap">M7-4 beds,</span>{" "}
+                    <span className="whitespace-nowrap">M8-4 beds,</span>{" "}
+                    <span className="whitespace-nowrap">M9-4 beds,</span>{" "}
+                    <span className="whitespace-nowrap">1 bath each.</span>
+                  </li>
+                  <li>
+                    8. <b>Staff Quarters & Laundry</b>
+                  </li>
+                  <li>
+                    9. <b>Elk:</b>{" "}
+                    <span className="whitespace-nowrap">E1-4 beds,</span>{" "}
+                    <span className="whitespace-nowrap">E2-4 beds,</span>{" "}
+                    <span className="whitespace-nowrap">E3-4 beds,</span>{" "}
+                    <span className="whitespace-nowrap">E4-4 beds,</span>{" "}
+                    <span className="whitespace-nowrap">E5-4 beds,</span>{" "}
+                    <span className="whitespace-nowrap">1 bath each.</span>{" "}
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
